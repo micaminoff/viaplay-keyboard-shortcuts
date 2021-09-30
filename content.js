e20a8e28e3bd349d7bc26d6d2a236fea696cbee1
AminoffZ <55880031+AminoffZ@users.noreply.github.com>
2021-09-30 20:18:54 +0300

    Structuring
    Old features work fine but can't get arrow keys to work when loaded as extension... Pasting the code in console works fine.

--- content.js
+++ content.js
@@ -1,11 +1,27 @@
-/** Viaplay's player injects the controls as DOM elements when mouse moves so we need to trigger that */
+/* Viaplay's player injects the controls as DOM elements when mouse moves so we need to trigger that */
 const showUI = () => {
   document
     .querySelector(".scene")
     .dispatchEvent(new Event("mousemove", { bubbles: true }));
 };
 
-/** Listen to user pressing a button in the keyboard */
+let sessionID;
+/* We append this string to use react keys*/
+function setSessionID() {
+  sessionID = Object.keys(document.getElementById("react-mount")).find(key=>key.startsWith("__reactContainere$")).split("$")[1];
+}
+
+/* We check if document is ready */
+if (document.readyState !== "loading") {
+  setSessionID();
+} else {
+  /* If not we execute when ready */
+  document.addEventListener("DOMContentLoaded", function () {
+    setSessionID()
+  });
+}
+
+/* Listen to user pressing a button in the keyboard */
 document.addEventListener("keyup", (event) => {
   event.preventDefault();
 
@@ -53,67 +69,68 @@ document.addEventListener("keyup", (event) => {
     }
   }
 
-  return false;
-});
-
-/** Listen to user pressing a button in the keyboard (checks if held down) */
-document.addEventListener("keydown", (event) => {
-  event.preventDefault();
-
   /* If it's up, we want to increase volume (increments at 10%)*/
-  if (event.key === "ArrowUp") {
-    showUI();
-
-    /* We need to find __reactEventHandlers and __reactInternalInstance as it changes every time we load the player */
-    const reactInternalInstance = Object.keys(document.querySelector(".scene")).find(key=>key.startsWith("__reactInternalInstance$"));
-    const reactEventHandler = Object.keys(document.querySelector(".scene")).find(key=>key.startsWith("__reactEventHandlers$"));
-    
-    /* We find where to trigger the audio slider*/
-    const toggleAudioControl = document.querySelector(".audio-control")[reactInternalInstance].return.pendingProps;
-
-    /* We open audio control */
-    toggleAudioControl.onMouseEnter();
-
-    /* We find where to change volume */
-    const volume =  document.querySelector(".audio-slider")[reactEventHandler].children.props;
-
-    /* Make sure audio doesn't go above 1 (100%) */
-    if ((volume.value + 0.1) > 1) {
-      volume.onChange(1);
-    } else {
-      volume.onChange(volume.value + 0.1)
-    }
-
-    /* We close audio control */
-    toggleAudioControl.onMouseLeave();
-  }
-
-    /* If it's down, we want to decrease volume (increments at 10%)*/
-    if (event.key === "ArrowDown") {
+  if (event.key === "ArrowUp" && !!sessionID) {
+    setTimeout(() => {
       showUI();
-  
-      /* We need to find __reactEventHandlers and __reactInternalInstance as it changes every time we load the player */
-      const reactInternalInstance = Object.keys(document.querySelector(".scene")).find(key=>key.startsWith("__reactInternalInstance$"));
-      const reactEventHandler = Object.keys(document.querySelector(".scene")).find(key=>key.startsWith("__reactEventHandlers$"));
 
-      /* We find where to trigger the audio slider */
+      /* We need to find __reactEventHandlers and __reactInternalInstance as it changes every time we load the player */
+      const reactInternalInstance = "__reactInternalInstance$" + sessionID;
+      const reactEventHandlers = "__reactEventHandlers$" + sessionID;
+
+      /* We find where to trigger the audio slider*/
       const toggleAudioControl = document.querySelector(".audio-control")[reactInternalInstance].return.pendingProps;
 
       /* We open audio control */
       toggleAudioControl.onMouseEnter();
 
       /* We find where to change volume */
-      const volume =  document.querySelector(".audio-slider")[reactEventHandler].children.props;
-      
-      /* Make sure audio doesn't go below 0 */
-      if ((volume.value - 0.1) < 0) {
-        volume.onChange(0);
+      const volumeLevel =  document.querySelector(".audio-slider")[reactEventHandlers].children.props;
+
+      /* Make sure audio doesn't go above 1 (100%) */
+      if ((volumeLevel.value + 0.1) > 1) {
+        volumeLevel.onChange(1);
       } else {
-        volume.onChange(volume.value - 0.1)
+        volumeLevel.onChange(volumeLevel.value + 0.1)
       }
-      
+
       /* We close audio control */
       toggleAudioControl.onMouseLeave();
+
+    }, 10);
+  } else {
+
+  }
+
+    /* If it's down, we want to decrease volume (increments at 10%)*/
+    if (event.key === "ArrowDown" && !!sessionID) {
+      setTimeout(() => {
+        showUI();
+
+        /* We need to find __reactEventHandlers and __reactInternalInstance as it changes every time we load the player */
+        const reactInternalInstance = "__reactInternalInstance$" + sessionID;
+        const reactEventHandlers = "__reactEventHandlers$" + sessionID;
+
+        /* We find where to trigger the audio slider */ 
+        const toggleAudioControl = document.querySelector(".audio-control")[reactInternalInstance].return.pendingProps;
+
+        /* We open audio control */
+        toggleAudioControl.onMouseEnter();
+
+        /* We find where to change volume */
+        const volumeLevel =  document.querySelector(".audio-slider")[reactEventHandlers].children.props;
+        
+        /* Make sure audio doesn't go below 0 */
+        if ((volumeLevel.value - 0.1) < 0) {
+          volumeLevel.onChange(0);
+        } else {
+          volumeLevel.onChange(volumeLevel.value - 0.1)
+        }
+        
+        /* We close audio control */
+        toggleAudioControl.onMouseLeave();
+  
+      }, 10);
     }
 
   return false;
